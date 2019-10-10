e3d019f06d348c3a76bd4dae5a4d61f5ecc77155
Mike Jenkins <kinetic@icloud.com>
2019-10-10 16:09:26 -0400

    Added changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
+    <PrototypeBanner/>
     <div class="container">
-          <Calculator msg="Accommodation and Car Rental Directory"/>
+      <Calculator msg="Accommodation and Car Rental Directory"/>
     </div>
   </div>
 </template>
 
 <script>
+import PrototypeBanner from './components/PrototypeBanner.vue'
 import Calculator from './components/Calculator.vue'
 import Vue from 'vue'
 import Autocomplete from '@trevoreyre/autocomplete-vue'
@@ -17,7 +19,8 @@ Vue.use(Autocomplete)
 export default {
   name: 'app',
   components: {
-    Calculator
+    Calculator,
+    PrototypeBanner,
   }
 }
 </script>
